1216776a4edd257ae49f3b46787a0fdb9fc7e49d
Garima Rajput <garimarajput748@gmail.com>
2023-01-30 19:35:25 +0530

    file upload method

---
+++ phpPractice/fileupload.php
@@ -0,0 +1,86 @@
+<?php
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+   
+ // Check if image file is a actual image or fake image   
+
+if(isset($_POST['submit']) && $_POST['submit'] == 'UPLOAD'){
+    if(isset($imageFileType) && !empty($imageFileType)){
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+          } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+            return;
+          }
+    }
+    else {
+        echo 'Please Select Image';
+        return;
+    }
+}
+
+// Allow certain file formats
+    if(!empty($imageFileType)){
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "webp" ) {
+        echo "Sorry, only JPG, JPEG, PNG & WEBP files are allowed.";
+        $uploadOk = 0;
+        return;
+        }
+    }
+
+// Check file size
+    if ($_FILES["fileToUpload"]["size"] > 16000000) {
+        echo "Sorry, your file is too large. Max Size can be 2 MB";
+        $uploadOk = 0;
+        return;
+    }
+
+// Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+        return;
+    }
+
+// if everything is ok, try to upload file
+  else {
+    // var_dump($_FILES["fileToUpload"]["tmp_name"]); exit;
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+      echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+      header('Location: '.$_SERVER['PHP_SELF']);
+    } else {
+      echo "Sorry, there was an error uploading your file.";
+      return;
+    }
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <title>File Upload</title>
+</head>
+<body>
+    <div class="container mt-5">
+        <h4>Upload File </h4>
+        <form class="w-50" method="POST" enctype="multipart/form-data">
+            <!-- <label for="fileName">FileName::</label> -->
+            <input  type="file" id="file" name="fileToUpload" class="form-control" required>
+            <input type="submit" name="submit" class="btn btn-success mt-4" value="UPLOAD">
+            <input type="reset" class="btn btn-danger mt-4 ms-5" value="RESET">
+        </form>
+    </div>
+</body>
+</html>
